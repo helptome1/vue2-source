0d59aa573b6bbf6edc8f8d8a814fba8f0ad0e433
hezg@sunwayworld.com <hzgdeemail@163.com>
2022-06-14 17:31:57 +0800

    完成数组的监听。

---
+++ src/observe/array.js
@@ -0,0 +1,47 @@
+// 获取 数组的原型
+let oldArrayProto = Array.prototype;
+
+// 拷贝一份数组原型，修改数组方法，不会影响Array的方法。
+export let newArrayProto = Object.create(oldArrayProto);
+
+// 这里是改变元素组的方法。
+let methods = [
+  'push',
+  'pop',
+  'shift',
+  'unshift',
+  'reverse',
+  'sort',
+  'splice'
+]
+
+methods.forEach(method => {
+  // arr.push([1,2,3])
+  newArrayProto[method] = function (...args) {
+    // 这里重写了数组的方法。
+    // 调用原型上的方法。
+    // 这里的this指向的是谁调用的，this指向arr
+    const result= oldArrayProto[method].call(this, ...args);
+    // 需要对新增的数据再次劫持。
+    let inserted;
+    let ob = this.__ob__;
+    switch (method) {
+      case 'push':
+      case 'unshift':
+        inserted = args
+        break;
+      case 'splice':
+        inserted = args.slice(2)
+        break;
+      default:
+        break;
+    }
+    if(inserted) {
+      // 对新增的数组内容再次劫持
+      ob.observeArray(inserted)
+    }
+    return result
+  }
+})
+
+
--- src/observe/index.js
+++ src/observe/index.js
@@ -1,7 +1,18 @@
+import { newArrayProto } from "./array"
+
 class Observe {
   constructor(data) {
     // Object.defineProperty只能劫持已经存在的属性，不能劫持新增的属性（为此vue会新增一些方法例如：$set, $delete）
+    // 把Observe挂载到__ob__上，做一个标识（识别是否已经监听），同时可以使用walk和observeArray方法。
+    // 使用Object.defineProperty方法加上__ob__属性，表示已经监听，并且让这个属性不可枚举，防止死循环。
+    Object.defineProperty(data, '__ob__', {
+      value: this,
+      enumerable: false,
+    })
     if (Array.isArray(data)) {
+      // 为了解决数组中新增的属性不能劫持的问题，需要重写素组方法，并且不可以影响之前的数组方法。
+      // 新建一个array.js实现新增属性的方法重写。
+      data.__proto__ = newArrayProto
       //  修改数组的每一项时进行修改，不管是基本数据类型，还是引用数据类型。
       this.observeArray(data)
     } else {
@@ -47,6 +58,9 @@ export function observe(data) {
   // 对这个对象进行劫持
   if (typeof data !== 'object' || data === null) {
     return //只对对象进行劫持
+  }
+  if (data.__ob__ instanceof Observe) {
+    return data.__ob__;
   }
   // 如果一个对象已经被劫持了，那么它就不需要再次被劫持。（判断方法是：添加一个实例，用实例来判断）
   // 新增了Observer实例，判断这个对象是否已经被劫持
